52385ee3534bdd0d020f59fddda482e2b6c1744f
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 16:25:58 +0500

    Update basic27.js
    Реализована вставка элемента в массив на заданную позицию.

--- src/basic/basic27.js
+++ src/basic/basic27.js
@@ -1,7 +1,4 @@
-/* 
-	Реализуйте функцию insertAtPosition(arr, pos, elem), которая вставляет новый элемент в указанную позицию массива с помощью методов slice и concat.
-*/
-
 export function insertAtPosition(arr, pos, elem) {
-  return arr;
+  // Вырезаем левую часть до позиции, объединяем с элементом и затем с правой частью
+  return arr.slice(0, pos).concat([elem], arr.slice(pos));
 }
