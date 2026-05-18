d11917571de4914b289a68c4bea7c749a5740087
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 15:36:22 +0500

    Update basic23.js
    Реализована функция удаления специальных символов из строки.

--- src/basic/basic23.js
+++ src/basic/basic23.js
@@ -1,9 +1,12 @@
-/* 
-	Напишите функцию strip_special_chars(text), которая удаляет из текста все специальные символы и оставляет только буквы и пробелы.
-	Подсказка: для проверки символа можно использовать константу 
-	const allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ';
-*/
-
 export function strip_special_chars(text) {
-	return "";
+  const allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+  let result = '';
+  // Оставляем только те символы, которые входят в строку разрешенных символов
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    if (allowed.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
 }
