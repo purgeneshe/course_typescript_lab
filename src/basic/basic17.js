67cb43011ea88398180ac46c26090685873d9229
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 14:17:27 +0500

    Update basic17.js
    Реализовано удаление дубликатов из массива с сохранением порядка.

--- src/basic/basic17.js
+++ src/basic/basic17.js
@@ -1,9 +1,10 @@
-/* 
-	Напишите функцию remove_duplicates(arr), удаляющую дублирующиеся значения из массива, сохраняя порядок остальных элементов.
-	Подсказка: можно использовать функцию includes.
-*/
-
 export function remove_duplicates(arr) {
-    const unique = [];
-    return unique;
+  const unique = [];
+  // обавляем в unique только те элементы, которых там ещё нет
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
 }
