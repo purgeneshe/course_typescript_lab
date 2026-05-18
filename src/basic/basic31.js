c6399ff793654c033b18f363d911b61ac3d767f9
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 17:40:27 +0500

    Update basic31.js
    Реализовано транспонирование матрицы.

--- src/basic/basic31.js
+++ src/basic/basic31.js
@@ -1,7 +1,20 @@
-/* 
-	Реализуйте функцию transposeMatrix(matrix), которая транспонирует матрицу (меняет строки и столбцы местами).
-*/
+export function transposeMatrix(matrix) {
+  const R = matrix.length;
+  if (R === 0) return [];
 
-export function transposeMatrix(matrix) {	
-	return [[]];
+  const C = matrix[0].length;
+  if (C === 0) return [];
+
+  const transposed = [];
+  // Обходим каждый столбец исходной матрицы
+  for (let c = 0; c < C; c++) {
+    const row = [];
+    // Обходим каждую строку для текущего столбца c
+    for (let r = 0; r < R; r++) {
+      row.push(matrix[r][c]);
+    }
+    transposed.push(row);
+  }
+
+  return transposed;
 }
