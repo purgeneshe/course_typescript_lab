0241732c5cb963c042ff21e7f941afc7f29ea98b
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 20:47:07 +0500

    Update basic39.js
    Реализована функция all без использования встроенного метода every.

--- src/basic/basic39.js
+++ src/basic/basic39.js
@@ -1,9 +1,9 @@
-/* 
-	Реализовать функцию all, которая принимает массив и функцию-callback. Callback принимает один элемент массива и возвращает true или false. Функция all должна вернуть true, если все элементы массива вернули true в callback. Нельзя использовать метод массивов every.
-*/
-
 export function all(arr, callback) {
-
-	return true;
+  // Проходим по массиву и возвращаем false, если хотя бы один элемент не удовлетворяет условию
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i])) {
+      return false;
+    }
+  }
+  return true;
 }
-
