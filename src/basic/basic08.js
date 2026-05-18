8b096ba14365cce62e26607c2d2f3661176964ce
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 12:49:47 +0500

    Update basic08.js
    Реализована функция суммирования цифр числа без преобразования в строку.

--- src/basic/basic08.js
+++ src/basic/basic08.js
@@ -1,8 +1,9 @@
-/* 
-	Реализовать функцию sum_digits(num), которая суммирует цифры целого положительного числа num. Например, sum_digits(1234) вернёт 1 + 2 + 3 + 4 = 10. Функция должна использовать цикл while и не должна использовать преобразования числа в строку.
-*/
-
 export function sum_digits(num) {
-  return 0;
+  let sum = 0;
+  let currentNum = num;
+  while (currentNum > 0) {
+    sum += currentNum % 10; // Прибавляем последнюю цифру
+    currentNum = Math.floor(currentNum / 10); // Отбрасываем последнюю цифру
+  }
+  return sum;
 }
-
