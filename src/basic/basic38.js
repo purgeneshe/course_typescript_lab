13f99f36796ca2c3f4c97a69553ca3ef46d773dc
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 20:39:19 +0500

    Update basic38.js
    Реализована функция some без использования встроенного метода массивов.

--- src/basic/basic38.js
+++ src/basic/basic38.js
@@ -1,10 +1,11 @@
-/* 
-	Реализовать функцию some, которая принимает массив и функцию-callback. Callback принимает один элемент массива и возвращает true или false. Функция some должна вернуть true, если хотя бы один элемент массива вернул true в callback. Нельзя использовать метод массивов some.
-*/
-
 export function some(arr, callback) {
-	return false;
+  // Проходим по массиву и возвращаем true, если хотя бы один элемент удовлетворяет условию
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      return true;
+    }
+  }
+  return false;
 }
-
 
 
