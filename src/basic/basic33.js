b02e0c2d4663c6b62702aa3a7afcb1d3679774b5
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 18:39:13 +0500

    Update basic33.js
    Реализовано обнуление строки и столбца матрицы по заданным индексам.

--- src/basic/basic33.js
+++ src/basic/basic33.js
@@ -1,7 +1,17 @@
-/* 
-	Реализуйте функцию zeroRowColumn(matrix, row, col), которая превращает в ноль всю строку и весь столбец матрицы, соответствующие указанным индексам.
-*/
-
 export function zeroRowColumn(matrix, row, col) {
-    return matrix;
+  const R = matrix.length;
+  if (R === 0) return matrix;
+  const C = matrix[0].length;
+
+  // Зануляем всю строку с индексом row
+  for (let c = 0; c < C; c++) {
+    matrix[row][c] = 0;
+  }
+
+  // Зануляем весь столбец с индексом col
+  for (let r = 0; r < R; r++) {
+    matrix[r][col] = 0;
+  }
+
+  return matrix;
 }
