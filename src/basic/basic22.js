7218ed295add02c4eaf83f767fe264d92c17fb3a
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 15:26:11 +0500

    Update basic22.js
    Реализована функция проверки слова на палиндром.

--- src/basic/basic22.js
+++ src/basic/basic22.js
@@ -1,8 +1,5 @@
-/* 
-	Реализуйте функцию palindrome_check(word), которая проверяет, является ли слово палиндромом (читается одинаково слева направо и справа налево).
-	Подсказка: используйте методы split и join для строк и метод reverse для массивов.
-*/
-
 export function palindrome_check(word) {
-	return true;
+  // Разворачиваем строку и сравниваем её с оригинальной
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
 }
