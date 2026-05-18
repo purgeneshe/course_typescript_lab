f0a59fd1a05d3e40d31026cc3d097d1695f68a21
KonyukhovDenis <deniseykon@gmail.com>
2026-05-18 18:04:05 +0500

    Update basic32.js
    Реализовано перемножение матриц.

--- src/basic/basic32.js
+++ src/basic/basic32.js
@@ -1,7 +1,24 @@
-/* 
-	Напишите функцию matrixMultiplication(A, B), выполняющую перемножение матриц А и В.
-*/
-
 export function matrixMultiplication(A, B) {
-  return [[1]];
+  const R_A = A.length;
+  const C_A = A[0].length;
+  const C_B = B[0].length;
+
+  const result = [];
+
+  // Внешний цикл по строкам матрицы A
+  for (let i = 0; i < R_A; i++) {
+    const row = [];
+    // Средний цикл по столбцам матрицы B
+    for (let j = 0; j < C_B; j++) {
+      let sum = 0;
+      // Внутренний цикл по элементам строки A и столбца B
+      for (let k = 0; k < C_A; k++) {
+        sum += A[i][k] * B[k][j];
+      }
+      row.push(sum);
+    }
+    result.push(row);
+  }
+
+  return result;
 }
