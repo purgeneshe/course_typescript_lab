4f011002e55258fadae7926dee640be78a1e6e96
KonyukhovDenis <deniseykon@gmail.com>
2026-05-20 19:00:06 +0500

    Update objects09.ts
    Реализован метод move для изменения координат объекта point.

--- src/objects/objects09.ts
+++ src/objects/objects09.ts
@@ -1,11 +1,9 @@
-/* 
-	Реализовать метод move в объекте point, сдвигающий точку на dx и dy.
-*/
-
 export const point = {
-	x: 0,
-	y: 0,
-	move: function (dx: number, dy: number) {
-	}
+  x: 0,
+  y: 0,
+  move: function (dx: number, dy: number) {
+    // Сдвигаем текущие координаты x и y на заданные величины dx и dy
+    this.x += dx;
+    this.y += dy;
+  },
 };
-
