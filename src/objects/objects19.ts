98e1e3690a9a8f96ae9d9e9265dcfbb00b65bfd2
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 01:19:57 +0500

    Update objects19.ts
    Реализован метод withTaxRate, возвращающий новый объект с taxAmount

--- src/objects/objects19.ts
+++ src/objects/objects19.ts
@@ -1,10 +1,11 @@
-/* 
-	Реализовать метод withTaxRate в объекте product, который возвращает новый объект, такого же типа как и исходный, но с добавленным полем taxAmount, равным произведению price и taxRate.
-*/
-
 export const product = {
     price: 100,
     withTaxRate(rate: number) {
-        return this;
-    }
+        // Возвращаем новый объект с копией свойств исходного и добавленным taxAmount = price * rate
+        return {
+            ...this,
+            taxAmount: this.price * rate,
+        };
+    },
 };
+
