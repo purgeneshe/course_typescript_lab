b5e18ca60f511250fdf5703c87bb589036dc0d60
KonyukhovDenis <deniseykon@gmail.com>
2026-05-20 17:20:54 +0500

    Update objects07.ts
    Реализована функция maxOf для поиска максимального элемента Comparable.

--- src/objects/objects07.ts
+++ src/objects/objects07.ts
@@ -1,12 +1,18 @@
-/* 
-	Реализовать функцию maxOf<T extends Comparable>(a: T[]): T, возвращающую максимальное значение в массиве.
-*/
-
 export type Comparable = {
-	greaterThan: (other: Comparable) => boolean
-}
+  greaterThan: (other: Comparable) => boolean;
+};
 
 export function maxOf<T extends Comparable>(a: T[]): T {
-	return a[0];
-	
+  if (a.length === 0) {
+    throw new Error("Array is empty");
+  }
+
+  let max = a[0];
+  // Находим максимальный элемент по отношению greaterThan
+  for (let i = 1; i < a.length; i++) {
+    if (a[i].greaterThan(max)) {
+      max = a[i];
+    }
+  }
+  return max;
 }
