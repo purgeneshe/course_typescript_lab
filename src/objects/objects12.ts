c25c9f5a4caa4bf75709012826036f00412d4662
KonyukhovDenis <deniseykon@gmail.com>
2026-05-20 20:00:16 +0500

    Update objects12.ts
    Стрелочная функция add заменена на обычный метод для исправления привязки this.

--- src/objects/objects12.ts
+++ src/objects/objects12.ts
@@ -1,14 +1,12 @@
-/* 
-	Функция add должна добавлять переданное значение к предыдущему значению, но она работает некорректно. Почему? Исправьте ошибку.
-*/
-
 const calculator = {
-    previousValue: 0,
-    add: (value: number) => {
-        return this.previousValue += value;
-    }
+  previousValue: 0,
+  add(value: number) {
+    // Стрелочные функции не имеют собственного контекста 'this'.
+    // Изменяем на обычный метод, чтобы 'this' указывал на объект calculator.
+    return this.previousValue += value;
+  }
 };
 
 export function add(value: number) {
-	return calculator.add(value);
+  return calculator.add(value);
 }
