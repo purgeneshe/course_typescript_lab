da050c820b1526fe2ff12612c55b44cf419d98ec
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 14:32:22 +0500

    Update classes06.ts
    Реализован статический метод getCurrentCount в классе Counter, возвращающий значение count

--- src/classes/classes06.ts
+++ src/classes/classes06.ts
@@ -1,13 +1,10 @@
-/* 
-	Напишите класс Counter, содержащий статическое свойство count, которое увеличивается на единицу при каждом создании нового экземпляра класса. Реализуйте статический метод getCurrentCount, возвращающий текущее значение счётчика.
-*/
-
 export class Counter {
     static count = 0;
     constructor() {
         Counter.count++;
     }
     static getCurrentCount() {
-
+        // Возвращаем текущее значение статического счётчика
+        return Counter.count;
     }
 }
