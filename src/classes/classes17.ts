5a3c7e57d02b9de1bf23f91e3798677ea56aa732
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 18:25:06 +0500

    Update classes17.ts
    Добавлены методы moveTo и getMaxX для класса Square

--- src/classes/classes17.ts
+++ src/classes/classes17.ts
@@ -1,7 +1,3 @@
-/* 	
-	Реализуйте методы moveTo(передвигает нижний левый угол квадрата в указанную координату) и getMaxX (возвращает максимальное значение X координаты правого верхнего угла) класса Square
-*/
-
 export class Point {
   x: number;
   y: number;
@@ -26,10 +22,17 @@ export class Square implements Figure {
     this.point1 = point1;
     this.point2 = point2;
   }
-  moveTo(point: Point) {
-	
-  }
-  getMaxX() {
 
+  moveTo(point: Point) {
+    // Сохраняем размер квадрата и перемещаем point1, обновляя point2
+    const width = this.point2.x - this.point1.x;
+    const height = this.point2.y - this.point1.y;
+    this.point1 = point;
+    this.point2 = new Point(point.x + width, point.y + height);
+  }
+
+  getMaxX() {
+    // Возвращаем максимальную X-координату (правый верхний угол)
+    return this.point2.x;
   }
 }
