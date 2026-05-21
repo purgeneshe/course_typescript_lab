a2db193dea2e963f8aa41dac6e8074b634e94f06
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 20:27:36 +0500

    Update model03.ts
    Добавлен конструктор в Employee с name и position

--- src/model/model03.ts
+++ src/model/model03.ts
@@ -1,17 +1,17 @@
-/* 
-	Добавьте конструктор в класс Employee с дополнительным свойством position
-*/
-
-
 export class Person {
   name: string;
-  
+   
   constructor(name: string) {
     this.name = name;
   }
 }
 
-
 export class Employee extends Person {
-  
+  position: string;
+
+  constructor(name: string, position: string) {
+    // Вызываем конструктор родителя и добавляем position
+    super(name);
+    this.position = position;
+  }
 }
