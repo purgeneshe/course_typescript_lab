078f16f0c9ed9ebb9d38cca9f5f2a08cd6f062b7
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 14:17:02 +0500

    Update classes05.ts
    Реализован класс Task с перегрузкой конструкторов (title или title+done) и done по умолчанию false

--- src/classes/classes05.ts
+++ src/classes/classes05.ts
@@ -1,14 +1,11 @@
-/* 
-	Класс Task, позволяющий создать задачу либо простым текстом («Купить хлеб»), либо путем передачи названия и статуса.
-*/
-
-
 export class Task {
     title: string;
     done: boolean;
-    constructor(title: string);                  // Только название задачи
-    constructor(title: string, done: boolean);   // Название и статус завершения
+    constructor(title: string);
+    constructor(title: string, done: boolean);
     constructor(title: string, done?: boolean) {
-
+        // Инициализируем title и done (если done не передан - false по умолчанию)
+        this.title = title;
+        this.done = done ?? false;
     }
 }
