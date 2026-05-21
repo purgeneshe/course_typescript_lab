9f19061c4585fd000cf87ccb6bd2ed32be06f74c
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 18:01:54 +0500

    Update classes16.ts
    Добавлен класс Complex с конструктором и методом sum для комплексных чисел

--- src/classes/classes16.ts
+++ src/classes/classes16.ts
@@ -1,7 +1,3 @@
-/* 
-	Реализовать класс Complex (комплексное число) по описанию типа ComplexType (включить конструктор и метод sum).
-*/
-
 type ComplexType = { 
 	real: number; 
 	imag: number;
@@ -9,6 +5,17 @@ type ComplexType = {
 };
 
 export class Complex implements ComplexType {
+  real: number;
+  imag: number;
 
+  constructor(real: number, imag: number) {
+    this.real = real;
+    this.imag = imag;
+  }
+
+  // Возвращаем новый экземпляр Complex с суммой real и imag
+  sum(other: ComplexType): ComplexType {
+    return new Complex(this.real + other.real, this.imag + other.imag);
+  }
 }
 
