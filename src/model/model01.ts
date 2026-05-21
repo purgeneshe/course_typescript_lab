6c6401acf589de1a23e48fb795dda6a16f2f10c1
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 19:05:56 +0500

    Update model01.ts
    Добавлен класс Dog, наследующий Animal и реализующий bark()

--- src/model/model01.ts
+++ src/model/model01.ts
@@ -1,17 +1,22 @@
-/* 
-	Создать класс Dog, который наследует от Animal и реализует метод bark() (выводит "Woof!").
-*/
-
 export class Animal {
   name: string;
-  
+   
   constructor(name: string) {
     this.name = name;
   }
-  
+   
   makeSound(): void {
     console.log("Some generic sound");
   }
 }
 
-export class Dog 
+export class Dog extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+
+  // Реализуем метод bark, который выводит "Woof!"
+  bark(): void {
+    console.log("Woof!");
+  }
+}
