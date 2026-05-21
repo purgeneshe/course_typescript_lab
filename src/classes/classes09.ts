8c8af00721287e646790dbfff83e1876ab73bb06
KonyukhovDenis <deniseykon@gmail.com>
2026-05-21 14:59:55 +0500

    Update classes09.ts
    Добавлен статический discountRate и applyDiscount в Product, применяющий скидку ко всем объектам в массиве

--- src/classes/classes09.ts
+++ src/classes/classes09.ts
@@ -1,11 +1,9 @@
-/* 	
-	В классе Product нужно добавить статическое свойство discountRate, указывающее скидку на товары, и статический метод applyDiscount, применяющий скидку ко всей продукции.
-*/
-
 export class Product {
   
   name: string;
   price: number;
+  static discountRate = 0;
+
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
@@ -14,6 +12,9 @@ export class Product {
     console.log(`Продукт: ${this.name}, цена: ${this.price}`);
   } 
   static applyDiscount(products: Product[]) {
-  
+    // Применяем скидку discountRate ко всем продуктам, обновляя их price
+    for (const product of products) {
+      product.price = product.price * (1 - Product.discountRate);
+    }
   }
 }
