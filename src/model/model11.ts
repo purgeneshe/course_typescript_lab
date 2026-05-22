e4a6ffc00b2e5f11943f14d6b84ffdb14bb90473
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 17:44:58 +0500

    Update model11.ts
    Реализованы методы save и load в MemoryStorage

--- src/model/model11.ts
+++ src/model/model11.ts
@@ -1,14 +1,18 @@
-/* 
-	Реализуйте оба абстрактных метода в классе MemoryStorage
-*/
-
 abstract class DataStorage {
   abstract save(data: string): void;
   abstract load(): string;
 }
 
-
 export class MemoryStorage extends DataStorage {
   private data: string = "";
-  
+
+  // Сохраняем данные в приватное поле
+  save(data: string): void {
+    this.data = data;
+  }
+
+  // Возвращаем сохранённые данные
+  load(): string {
+    return this.data;
+  }
 }
