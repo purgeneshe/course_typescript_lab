1fe76cae0acbf007ac066ac4a8e718e8b1d9a152
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 16:38:45 +0500

    Update model05.ts
    Добавлено свойство discountPercentage и конструктор в DiscountedProduct

--- src/model/model05.ts
+++ src/model/model05.ts
@@ -1,16 +1,17 @@
-/* 
-	Добавьте свойство discountPercentage в класс DiscountedProduct, реализуйте контруктор этого класса.
-*/
-
 export class Product {
   price: number;
-  
+   
   constructor(price: number) {
     this.price = price;
   }
 }
 
-
 export class DiscountedProduct extends Product {
+  discountPercentage: number;
 
+  constructor(price: number, discountPercentage: number) {
+    // Вызываем конструктор родителя и сохраняем скидку
+    super(price);
+    this.discountPercentage = discountPercentage;
+  }
 }
