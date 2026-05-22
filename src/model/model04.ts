74b4008fbfe3148d18c7a5e6b180bbec66afb192
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 16:32:14 +0500

    Update model04.ts
    Переопределён draw в Circle с вызовом super.draw()

--- src/model/model04.ts
+++ src/model/model04.ts
@@ -1,14 +1,13 @@
-/* 
-	В классе Circle переопределите метод draw, чтобы он сначала вызывал метод draw у родительского класса Shape, а затем выводил в консоль "Drawing circle".
-*/
-
 export class Shape {
   draw(): void {
     console.log("Drawing shape");
   }
 }
 
-
 export class Circle extends Shape {
-
+  // Переопределяем draw: сначала вызываем super, затем выводим своё сообщение
+  override draw(): void {
+    super.draw();
+    console.log("Drawing circle");
+  }
 }
