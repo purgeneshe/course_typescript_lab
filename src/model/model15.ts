03673a62492f0c8cfaeaf20cf7053e7480a4afc3
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 18:58:00 +0500

    Update model15.ts
    Сохранено предусловие в withdraw PremiumAccount

--- src/model/model15.ts
+++ src/model/model15.ts
@@ -1,7 +1,3 @@
-/* 
-	Класс PremiumAccount не должен изменять предусловия на параметры метода withdraw.
-*/
-
 export class Account {
   withdraw(amount: number): void {
     if (amount <= 0) throw new Error("Amount must be positive");
@@ -9,10 +5,10 @@ export class Account {
   }
 }
 
-
 export class PremiumAccount extends Account {
   override withdraw(amount: number): void {
-    
+    // Сохраняем то же предусловие, что и в базовом классе
+    if (amount <= 0) throw new Error("Amount must be positive");
     console.log(`Premium withdrawal: ${amount}`);
   }
 }
