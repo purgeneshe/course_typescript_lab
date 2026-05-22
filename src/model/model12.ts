76e6a6c937e542007375bcfb26a7fabbe1694cf5
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 18:03:13 +0500

    Update model12.ts
    Добавлен статический square и реализован calculate в CircleArea

--- src/model/model12.ts
+++ src/model/model12.ts
@@ -1,21 +1,24 @@
-/* 
-	Добавьте в MathOperations статический метод square, который возвращает число в квадрате. Реализуйте абстрактный метод calculate в классе CircleArea, который возвращает площадь круга по формуле pi * r^2, используя статический метод square
-*/
-
 export abstract class MathOperations {
   static pi: number = 3.14159265359;
-  
+
+  // Статический метод для возведения числа в квадрат
+  static square(n: number): number {
+    return n * n;
+  }
+
   abstract calculate(): number;
-
 }
-
 
 export class CircleArea extends MathOperations {
   radius: number;
-  
+
   constructor(radius: number) {
     super();
     this.radius = radius;
   }
-  
+
+  // Вычисляем площадь круга: pi * (r^2), используя square
+  calculate(): number {
+    return MathOperations.pi * MathOperations.square(this.radius);
+  }
 }
