b3d8979be64810a71cfe08f90e766b49252edc0f
KonyukhovDenis <deniseykon@gmail.com>
2026-05-22 20:11:31 +0500

    Update model18.ts
    Добавлена примесь Loggable с методом log

--- src/model/model18.ts
+++ src/model/model18.ts
@@ -1,12 +1,9 @@
-/* 
-	Создайте примесь с методом log, которая выводит в консоль сообщение в формате "[LOG]: <сообщение>"
-*/
-
-
-
 export function Loggable<TBase extends new (...args: any[]) => any>(Base: TBase) {
   return class extends Base {
-
+    // Добавляем метод log
+    log(message: string): void {
+      console.log(`[LOG]: ${message}`);
+    }
   };
 }
 
