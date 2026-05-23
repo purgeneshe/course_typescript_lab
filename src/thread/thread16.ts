bee10df5390b3542035735a048187cbbdd5a55d4
KonyukhovDenis <deniseykon@gmail.com>
2026-05-23 20:02:11 +0500

    Update thread16.ts
    getAllUserInfo возвращает результаты всех запросов через Promise.allSettled

--- src/thread/thread16.ts
+++ src/thread/thread16.ts
@@ -1,9 +1,11 @@
-/* 
-	Дописать функцию getAllUserInfo, которая возвращает результаты всех запросов независимо от их успешности.
-*/
-
 import { fetchUser, fetchUserData, fetchUserPosts, fetchUserSettings } from "./promises";
 
 export function getAllUserInfo(id: number): Promise<Array<PromiseSettledResult<any>>> {
-  // TODO: Использовать Promise.allSettled для получения всех результатов
+  // Используем Promise.allSettled, чтобы получить результаты всех запросов, даже если некоторые упали
+  return Promise.allSettled([
+    fetchUser(id),
+    fetchUserData(id),
+    fetchUserPosts(id),
+    fetchUserSettings(id),
+  ]);
 }
