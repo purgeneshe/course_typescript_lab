d90fc58d032d2d03d0ad6db2d189505977216002
KonyukhovDenis <deniseykon@gmail.com>
2026-05-24 00:34:36 +0500

    Update thread23.ts
    parseNumberWithDetailedError использует mapErr для создания AppError

--- src/thread/thread23.ts
+++ src/thread/thread23.ts
@@ -1,16 +1,15 @@
-/* 
-	Используйте mapErr, чтобы преобразовать ошибку из строки в более описательный объект. При этом установите код ошибки в 400, а сообщение в `Parse error: ${error}`
-*/
-
 import type { Result } from "neverthrow";
 import { parseNumber } from "./fornever";
 
 interface AppError {
-	message: string;
-	code: number;
+  message: string;
+  code: number;
 }
 
 export const parseNumberWithDetailedError = (input: string): Result<number, AppError> => {
-	return parseNumber(input)
-	// Ваш код здесь (3-5 строк)
+  return parseNumber(input).mapErr((error) => ({
+    // Преобразуем строку ошибки в объект AppError с кодом 400
+    message: `Parse error: ${error}`,
+    code: 400,
+  }));
 }
