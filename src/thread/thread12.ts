a8ba1808d0efec0c18bc64eeeb44737545b25347
KonyukhovDenis <deniseykon@gmail.com>
2026-05-23 18:21:20 +0500

    Update thread12.ts
    Реализована getUserProfile с параллельным Promise.all трёх запросов

--- src/thread/thread12.ts
+++ src/thread/thread12.ts
@@ -1,15 +1,17 @@
-/* 
-	Дописать функцию getUserProfile, которая параллельно получает пользователя, его данные и настройки.
-*/
-
 import { fetchUser, fetchUserData, fetchUserSettings } from "./promises";
 import type { User } from "./thread07";
 
-export function getUserProfile(id: number): Promise<{
+export async function getUserProfile(id: number): Promise<{
   user: User;
   data: User;
   settings: { theme: string };
 }> {
-  // TODO: Реализовать параллельное выполнение трех запросов
+  // Выполняем три запроса параллельно с помощью Promise.all
+  const [user, data, settings] = await Promise.all([
+    fetchUser(id),
+    fetchUserData(id),
+    fetchUserSettings(id),
+  ]);
 
+  return { user, data, settings };
 }
