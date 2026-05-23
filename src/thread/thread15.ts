cc5931a33708a78274474d1b93e407e351b05646
KonyukhovDenis <deniseykon@gmail.com>
2026-05-23 19:31:51 +0500

    Update thread15.ts
    getFastestUserData теперь возвращает результат первого завершившегося запроса через Promise.race

--- src/thread/thread15.ts
+++ src/thread/thread15.ts
@@ -1,10 +1,9 @@
-/* 
-	Дописать функцию getFastestUserData, которая возвращает результат первого завершившегося промиса (пользователь или данные пользователя).
-	Подсказка: используется Promise.race
-*/
-
 import { fetchUser, fetchUserData, type User } from "./promises";
 
 export function getFastestUserData(id: number): Promise<User> {
-  // TODO: Использовать Promise.race для получения самого быстрого результата
+  // Используем Promise.race, чтобы вернуть результат первого завершившегося запроса
+  return Promise.race([
+    fetchUser(id),
+    fetchUserData(id),
+  ]);
 }
